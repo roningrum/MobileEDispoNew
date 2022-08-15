bf52460c393366e85e8f169041ecc4a05f2e4275
Ota <rizkioktavianingrum@gmail.com>
2022-08-15 18:17:46 +0700

    Initial commit

---
+++ app/src/main/java/id/go/dinkes/mobileedisponew/AgendaInternal.kt
@@ -0,0 +1,11 @@
+package id.go.dinkes.mobileedisponew
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AgendaInternal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agenda_internal)
+    }
+}
